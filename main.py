d809e17246668eeed95eda3ffffc07a227abb72a
KimSangHwi <blatherr@gmail.com>
2024-01-25 14:42:01 +0900

    세 번째 커밋 곱하기 추가

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ def add_func(n1, n2):
 def subtract_func(n1, n2):
     return n1-n2
 
+def multi_func(n1, n2):
+    return n1*n2
+
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
 
 ## 메인 코드부
 add_res=add_func(num1, num2)
 sub_res=subtract_func(num1, num2)
+mul_res=multi_func(num1, num2)
 print("Addition : ", num1, "+", num2, "=", add_res)
 print("Subtraction : ", num1, "-", num2, "=", sub_res)
+print("Multiply : ", num1, "*", num2, "=", mul_res)
