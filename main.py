50b974a845664c8644e2a712cf6098cc21af8827
KimSangHwi <blatherr@gmail.com>
2024-01-25 17:34:38 +0900

    커밋5 제곱함수 추가

--- main.py
+++ main.py
@@ -11,15 +11,20 @@ def multi_func(n1, n2):
 def div_func(n1, n2):
     return n1/n2
 
+def square_func(n1, n2):
+    return pow(n1, n2)
+
 ## 전역 변수부
-num1, num2, res = 100, 200, 0
+num1, num2, res = 100, 2, 0
 
 ## 메인 코드부
 add_res=add_func(num1, num2)
 sub_res=subtract_func(num1, num2)
 mul_res=multi_func(num1, num2)
 div_res=div_func(num1, num2)
+sqr_res=square_func(num1, num2)
 print("Addition : ", num1, "+", num2, "=", add_res)
 print("Subtraction : ", num1, "-", num2, "=", sub_res)
 print("Multiply : ", num1, "*", num2, "=", mul_res)
 print("Division : ", num1, "/", num2, "=", div_res)
+print("Square : ", num1, "^", num2, "=", sqr_res)
