0df4c3aa70998d5890759dffee28ffff0097656b
KimSangHwi <blatherr@gmail.com>
2024-01-25 14:43:09 +0900

    네 번째 커밋 나누기 추가

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def subtract_func(n1, n2):
 def multi_func(n1, n2):
     return n1*n2
 
+def div_func(n1, n2):
+    return n1/n2
+
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
 
@@ -15,6 +18,8 @@ num1, num2, res = 100, 200, 0
 add_res=add_func(num1, num2)
 sub_res=subtract_func(num1, num2)
 mul_res=multi_func(num1, num2)
+div_res=div_func(num1, num2)
 print("Addition : ", num1, "+", num2, "=", add_res)
 print("Subtraction : ", num1, "-", num2, "=", sub_res)
 print("Multiply : ", num1, "*", num2, "=", mul_res)
+print("Division : ", num1, "/", num2, "=", div_res)
